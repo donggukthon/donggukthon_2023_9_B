f67ae7de76b590634b599f38be69db38a50014fb
bbbang105 <2018111366@dgu.ac.kr>
2023-12-18 13:18:35 +0900

    # 3 feat : 유저 엔터티 생성

---
+++ backend/src/main/java/com/snowball/backend/entity/User.java
@@ -0,0 +1,32 @@
+package com.snowball.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "provider", nullable = false)
+    private String provider;
+
+    @Column(name = "provider_id", nullable = false)
+    private String providerId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "warm_snow", nullable = false)
+    private Long warmSnow;
+
+    @Column(name = "access_date", nullable = false)
+    private String accessDate;
+}
