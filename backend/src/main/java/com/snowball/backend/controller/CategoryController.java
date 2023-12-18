cd946dfcc41b95252bdf4a1e4d21d59c43fad752
bbbang105 <2018111366@dgu.ac.kr>
2023-12-18 13:36:19 +0900

    # 7 feat : 카테고리 컨트롤러 구현 & 테스트 결과 반환 api 구현

---
+++ backend/src/main/java/com/snowball/backend/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package com.snowball.backend.controller;
+
+import com.snowball.backend.dto.CategoryDto;
+import com.snowball.backend.entity.Category;
+import com.snowball.backend.service.CategoryService;
+import com.snowball.backend.util.TestResultUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test-result")
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final TestResultUtil testResultUtil;
+
+    @GetMapping
+    // 테스트 결과를 통해 카테고리 id를 반환하는 API
+    public CategoryDto.Response getTestResult(
+            @RequestParam(name = "result") String testResult) {
+        // 테스트 결과 도출
+        Long categoryId = testResultUtil.getCategoryIdByTestResult(testResult);
+        Category category = categoryService.getCategoryByCategoryId(categoryId);
+        Category fitCategory = categoryService.getCategoryByCategoryId(category.getFitCategoryId());
+
+        return new CategoryDto.Response(
+                new CategoryDto.Data(
+                        category.getCategoryId(),
+                        category.getCategoryName(),
+                        fitCategory.getCategoryId(),
+                        fitCategory.getCategoryName()),
+                200,
+                "success");
+    }
+}
